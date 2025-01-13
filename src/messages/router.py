c60121054e5586e9f0b0295501df0306884af062
Darkhan <darhanuva@gmail.com>
2025-01-13 15:38:51 +0500

    changed alembic structure

--- src/messages/router.py
+++ src/messages/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 
 from src.celery_app import app as celery_app, redis_client
 
-from src.storage import collection
+# from src.storage import collection
 from src.auth.router import get_current_user
 from src.user_message.schemas import TestData
 from src.user_message.dao import UsersMessageDAO
@@ -17,7 +17,7 @@ async def get_json(data: TestData | None = None, current_user: dict = Depends(ge
 
     added_message = await UsersMessageDAO.add(**user_message)
 
-    celery_app.send_task('calculate_text_lenght', (added_message.message, current_user["user_id"]))
+    # celery_app.send_task('calculate_text_lenght', (added_message.message, current_user["user_id"]))
 
     # await save_message_log(added_message.id)
     return {'status': 'ok'}
@@ -34,8 +34,8 @@ async def view_dashboard(current_user: dict = Depends(get_current_user)):
     return {"redis": redis_tasks_info, "rows": rows}
 
 
-async def save_message_log(message_id):
-    await collection.insert_one({"saved_message_id": f"{message_id}", "status": "ok"})
+# async def save_message_log(message_id):
+#     await collection.insert_one({"saved_message_id": f"{message_id}", "status": "ok"})
 
 
 def get_tasks_count(user_id: int | str) -> dict:
